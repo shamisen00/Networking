79c3f74d31e5fc3cbaea8701dafa9bea004e92f3
yuki.kobayashi <ykobayashi134@gmail.com>
2022-04-22 23:56:07 +0900

    clean start.sh

---
+++ snippet/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+apt-get update
+apt-get -y install \
+    bash \
+    coreutils \
+    grep \
+    iproute2 \
+    iputils-ping \
+    traceroute \
+    tcpdump \
+    bind9-dnsutils \
+    dnsmasq-base \
+    netcat-openbsd \
+    python3 \
+    curl \
+    wget \
+    iptables \
+    procps \
+    isc-dhcp-client
